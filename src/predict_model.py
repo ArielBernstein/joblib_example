7b10eb60f9dc147c8181a2587cc9a6e5a700a36b
Ariel Bernstein <arbr532@gmail.com>
2026-03-21 20:30:40 +0200

    add models folder

--- src/predict_model.py
+++ src/predict_model.py
@@ -2,9 +2,6 @@ import joblib
 import numpy as np
 import pandas as pd
 from sklearn.metrics import mean_squared_error, r2_score
-import sys
-
-sys.path.append("..")
 from utility import plot_settings
 from utility.visualize import plot_predicted_vs_true, regression_scatter, plot_residuals
 
